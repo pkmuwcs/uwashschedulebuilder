680a4c8d0ffa2609a4c1f0b04b4a5caaaa708a7f
pkmuwcs <120143929+pkmuwcs@users.noreply.github.com>
2023-11-14 01:16:36 -0800

    Add files via upload

---
+++ client/components/pages/Home.js
@@ -0,0 +1,13 @@
+import '../../App.css';
+import MainSection from '../MainSection';
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+        <MainSection />
+        </>
+    );
+}
+
+export default Home;
